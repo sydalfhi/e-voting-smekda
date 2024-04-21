06ab64b4404f70b3c059252f3100d5ae56eab5a7
Syaid Alfarishi <syaidwork@gmail.com>
2024-04-21 10:25:49 +0700

    add chart in dashboard

--- resources/views/pages/backend/dashboard.blade.php
+++ resources/views/pages/backend/dashboard.blade.php
@@ -28,33 +28,132 @@
 
 
                 <div
-                    class="flex flex-col items-center justify-center mt-10 space-y-10 md:flex-row md:space-y-0 md:justify-between md:pr-10 xl:pr-16 ">
+                    class="flex flex-col items-center justify-center mt-10 space-y-10 md:flex-row md:space-y-0 md:justify-between md:pr-10 xl:pr-10 md:space-x-6">
 
-                    <div>
-                        <div class="loader">
-                            <div class="box1"></div>
-                            <div class="box2"></div>
-                            <div class="box3"></div>
-                        </div>
+
+                    <div class="w-full h-[220px] rounded-md bg-blue-500">
+
                     </div>
-                    <div>
-                        <div class="loader">
-                            <div class="box1"></div>
-                            <div class="box2"></div>
-                            <div class="box3"></div>
+
+
+                    <div class="min-w-[450px] flex items-center justify-center p-5 space-x-10 border rounded-md shadow ">
+                        <div>
+                            <div class="w-[190px] h-[190px]  grid place-items-center ">
+                                <canvas id="semuaDataPemilih"></canvas>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <div class="loader">
-                            <div class="box1"></div>
-                            <div class="box2"></div>
-                            <div class="box3"></div>
+
+                        <div>
+                            <ul class="list-disc">
+                                <li>Data Pemilih : 800</li>
+                                <li>Sudah Memilih : 600</li>
+                                <li>Belum Memilih : 200</li>
+                            </ul>
                         </div>
                     </div>
 
                 </div>
+
+                <div class="grid grid-cols-12 mt-10 gap-x-10">
+                    <div class="col-span-4">
+                        <canvas id="donat_paslon"></canvas>
+                    </div>
+                    <div class="col-span-8">
+                        <canvas id="bar-paslon"></canvas>
+                    </div>
+                </div>
+
+
+
+
+
             </div>
         </div>
 
     </section>
+
+    <section class="mt-20">
+        @include('components.fragment.footer')
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        let polarPemilih = document.getElementById('semuaDataPemilih').getContext('2d');
+        const data_pemmilih = {
+            labels: false,
+            datasets: [{
+                label: ['Data Pemilih', 'Sudah Memilih', 'Belum Memilih'],
+                data: [800, 600, 200],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(75, 192, 192)',
+                    'rgb(255, 205, 86)',
+                ]
+            }]
+        };
+
+
+        let myChart = new Chart(polarPemilih, {
+            type: 'polarArea',
+            data: data_pemmilih,
+            options: {}
+        });
+
+
+
+        let dougnut_Paslon = document.getElementById('donat_paslon').getContext('2d');
+        const doughnut_paslon = {
+            label: ['Data Pemilih', 'Sudah Memilih', 'Belum Memilih'],
+            datasets: [{
+                labels: 'Suara',
+                data: [800, 600, 200],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(75, 192, 192)',
+                    'rgb(255, 205, 86)',
+                ]
+            }]
+        };
+
+
+        let myChart_1 = new Chart(dougnut_Paslon, {
+            type: 'doughnut',
+            data: doughnut_paslon,
+        });
+
+
+
+
+
+        let bar_paslon = document.getElementById('bar-paslon').getContext('2d');
+        const bar_paslon_data = {
+            labels: [
+                'Paslon 01',
+                'Paslon 02',
+                'Belum Memilih'
+            ],
+            datasets: [{
+                label: 'Data SUara Paslon',
+                data: [404, 310, 90],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+            }]
+        };
+
+
+        let myChart2 = new Chart(bar_paslon, {
+            type: 'bar',
+            data: bar_paslon_data,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            },
+        });
+    </script>
 @endsection
