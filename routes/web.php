5e528f108a69126a38402f5f01f1c23fd015c747
TracerStudySmekdapapa <88016676+Thebibii@users.noreply.github.com>
2024-04-26 23:01:42 +0700

    feat: middleware IsAdmin

--- routes/web.php
+++ routes/web.php
@@ -24,21 +24,12 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-/* Route::get('/dashboard', function () {
-    $count = [
-        'user' => User::get()->where('role', 'user')->count(),
-        'statusTrue' => User::get()->where('role', 'user')->where('status', 'Y')->count(),
-        'statusFalse' => User::get()->where('role', 'user')->where('status', 'N')->count(),
-    ];
-    return view('pages.backend.dashboard', compact('count'));
-})->middleware(['auth', 'verified'])->name('dashboard'); */
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::get('/kandidat', [KandidatController::class, 'index'])->name('kandidat.index');
