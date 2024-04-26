28164641050fb56c662509a96de701d0fdd017d0
TracerStudySmekdapapa <88016676+Thebibii@users.noreply.github.com>
2024-04-26 14:33:19 +0700

    feat: terminal

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Recca0120\Terminal\TerminalServiceProvider::class,
     ])->toArray(),
 
     /*
